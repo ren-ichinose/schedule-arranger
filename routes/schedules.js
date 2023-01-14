c6aa1f6f55ca8ab4011c7b375f2f2e996225d26a
Ren Ichinose <kamatani.toru@gmail.com>
2023-01-15 02:30:35 +0900

    フォームに入力された値をDBへ保存する機能実装 #7

--- routes/schedules.js
+++ routes/schedules.js
@@ -2,14 +2,31 @@
 const express = require('express');
 const router = express.Router();
 const authenticationEnsurer = require('./authentication-ensurer');
+const { v4: uuidv4 } = require('uuid');
+const Schedule = require('../models/schedule');
+const Candidate = require('../models/candidate');
 
 router.get('/new', authenticationEnsurer, (req, res, next) => {
   res.render('new', { user: req.user });
 });
 
-router.post('/', authenticationEnsurer, (req, res, next) => {
-  console.log(req.body); // TODO 予定と候補を保存する実装をする
-  res.redirect('/');
+router.post('/', authenticationEnsurer, async (req, res, next) => {
+  const scheduleId = uuidv4();
+  const updatedAt = new Date();
+  const schedule = await Schedule.create({
+    scheduleId: scheduleId,
+    scheduleName: req.body.scheduleName.slice(0, 255) || '（名称未設定）',
+    memo: req.body.memo,
+    createdBy: req.user.id,
+    updatedAt: updatedAt
+  });
+  const candidateNames = req.body.candidates.trim().split('\n').map((s) => s.trim()).filter((s) => s !== "");
+  const candidates = candidateNames.map((c) => { return {
+    candidateName: c,
+    scheduleId: schedule.scheduleId
+  };});
+  await Candidate.bulkCreate(candidates);
+  res.redirect('/schedules/' + schedule.scheduleId);
 });
 
 module.exports = router;
