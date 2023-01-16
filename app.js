5e73eeb699b50c71a020954b2e256439b176678f
Ren Ichinose <kamatani.toru@gmail.com>
2023-01-17 00:39:34 +0900

    render への公開の為の調整 #30

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ User.sync().then(async () => {
 });
 
 const GitHubStrategy = require('passport-github2').Strategy;
-const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID_ENV;
-const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRETS_ENV;
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -42,7 +42,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:8000/auth/github/callback'
+  callbackURL: process.env.CALLBACK_URL
 },
   function (accessToken, refreshToken, profile, done) {
     process.nextTick(async function () {
